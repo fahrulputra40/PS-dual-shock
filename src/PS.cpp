68fd21a1a3905737b4483c56697da7dac6b67264
Fahrul putra pratama <33424526+fahrulputra40@users.noreply.github.com>
2019-10-10 05:28:54 +0700

    Add files via upload

---
+++ src/PS.cpp
@@ -0,0 +1,347 @@
+#include "PS.h"
+
+void delayForMicros(unsigned long delay_) {
+  delay_ = delay_ < 0 ? 0 : delay_;
+  unsigned long delayMicros = micros();
+  while (micros() - delayMicros < delay_) {
+
+  }
+}
+
+void delayForMillis(unsigned long delay_) {
+  delay_ = delay_ < 0 ? 0 : delay_;
+  unsigned long delayMillis = millis();
+  while (millis() - delayMillis < delay_) {
+
+  }
+}
+
+static byte enter_config[] = {0x01, 0x43, 0x00, 0x01, 0x00};
+static byte set_mode[] = {0x01, 0x44, 0x00, 0x01, 0x03, 0x00, 0x00, 0x00, 0x00};
+static byte exit_config[] = {0x01, 0x43, 0x00, 0x00, 0x5A, 0x5A, 0x5A, 0x5A, 0x5A};
+
+PS::PS() {
+
+}
+
+#ifdef __AVR__
+inline void  PS::CLK_SET(void) {        //set high pin CLK
+  register uint8_t old_sreg = SREG;
+  cli();
+  *_clk_oreg |= _clk_mask;
+  SREG = old_sreg;
+}
+
+inline void  PS::CLK_CLR(void) {        //reset pin CLK
+  register uint8_t old_sreg = SREG;
+  cli();
+  *_clk_oreg &= ~_clk_mask;
+  SREG = old_sreg;
+}
+
+inline void  PS::CMD_SET(void) {        //set pin CMD
+  register uint8_t old_sreg = SREG;
+  cli();
+  *_cmd_oreg |= _cmd_mask; // SET(*_cmd_oreg,_cmd_mask);
+  SREG = old_sreg;
+}
+
+inline void  PS::CMD_CLR(void) {        //set pin CMD
+  register uint8_t old_sreg = SREG;
+  cli();
+  *_cmd_oreg &= ~_cmd_mask; // SET(*_cmd_oreg,_cmd_mask);
+  SREG = old_sreg;
+}
+
+inline void  PS::ATT_SET(void) {        //set pin enable
+  register uint8_t old_sreg = SREG;
+  cli();
+  *_att_oreg |= _att_mask ;
+  SREG = old_sreg;
+}
+
+inline void PS::ATT_CLR(void) {         //reset pin enable
+  register uint8_t old_sreg = SREG;
+  cli();
+  *_att_oreg &= ~_att_mask;
+  SREG = old_sreg;
+}
+
+inline bool PS::DAT_CHK(void) {       //get data (bit)
+  return (*_dat_ireg & _dat_mask) ? true : false;
+}
+
+
+#else
+
+inline void  PS::CLK_SET(void) {        //set high pin CLK
+  digitalWrite(this->clk, HIGH);
+}
+
+inline void  PS::CLK_CLR(void) {        //reset pin CLK
+  digitalWrite(this->clk, LOW);
+}
+
+inline void  PS::CMD_SET(void) {        //set pin CMD
+  digitalWrite(this->cmd, HIGH);
+}
+
+inline void  PS::CMD_CLR(void) {        //set pin CMD
+  digitalWrite(this->cmd, low);
+}
+
+inline void  PS::ATT_SET(void) {        //set pin enable
+  digitalWrite(this->att, HIGH);
+}
+
+inline void PS::ATT_CLR(void) {         //reset pin enable
+  digitalWrite(this->att, LOW);
+}
+
+inline bool PS::DAT_CHK(void) {       //get data (bit)
+  return (digitalRead(dat) == HIGH) ? true : false;
+}
+
+#endif
+
+unsigned char PS::_gamepad_shiftinout (char byte) {
+  unsigned char tmp = 0;
+  for (unsigned char i = 0; i < 8; i++) {
+    if (CHK(byte, i)) CMD_SET();
+    else CMD_CLR();
+
+    CLK_CLR();
+    delayForMicros(CTRL_CLK);
+
+    //if(DAT_CHK()) SET(tmp,i);
+    if (DAT_CHK()) bitSet(tmp, i);
+
+    CLK_SET();
+#if CTRL_CLK_HIGH
+    delayForMicros(CTRL_CLK_HIGH);
+#endif
+  } // end for
+
+  CMD_SET();
+  delayForMicros(CTRL_BYTE_DELAY);
+  return tmp;
+}
+
+boolean PS::read_gamepad() {
+  NOP;
+  NOP;
+
+  double temp = millis() - last_read;
+
+  if (temp > 1500) //waited to long
+    reconfig_gamepad();
+
+  if (temp < read_delay) //waited too short
+    delayForMillis(read_delay - temp);
+
+  char dword[9] = {0x01, 0x42, 0, 0, 0, 0, 0, 0, 0};
+  byte dword2[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+  for (byte RetryCnt = 0; RetryCnt < 5; RetryCnt++) {
+    CMD_SET();
+    CLK_SET();
+    ATT_CLR(); // low enable joystick
+
+    NOP;
+    NOP;
+
+    for (int i = 0; i < 9; i++) {
+      PS2data[i] = _gamepad_shiftinout(dword[i]);
+    }
+
+    if (PS2data[1] == 0x79) { //if controller is in full data return mode, get the rest of data
+      for (int i = 0; i < 12; i++) {
+        PS2data[i + 9] = _gamepad_shiftinout(dword2[i]);
+      }
+    }
+
+    ATT_SET(); // HI disable joystick
+    if ((PS2data[1] & 0xf0) == 0x70)
+      break;
+
+    reconfig_gamepad(); // try to get back into Analog mode.
+    delayForMillis(read_delay);
+  }
+
+  if ((PS2data[1] & 0xf0) != 0x70) {
+    if (read_delay < 10)
+      read_delay++;   // see if this helps out...
+  }
+
+  last_buttons = buttons; //store the previous buttons states
+
+  buttons = *(uint16_t* )(PS2data + 3);
+
+  last_read = millis();
+  NOP;
+  NOP;
+
+  if ((PS2data[1] & 0xf0) == 0x70)return true;
+  else return false;
+}
+
+bool PS::config_gamepad(uint8_t clk, uint8_t cmd, uint8_t att, uint8_t dat) {
+
+#ifdef __AVR__
+  _clk_mask = digitalPinToBitMask(clk);
+  _clk_oreg = portOutputRegister(digitalPinToPort(clk));
+  _cmd_mask = digitalPinToBitMask(cmd);
+  _cmd_oreg = portOutputRegister(digitalPinToPort(cmd));
+  _att_mask = digitalPinToBitMask(att);
+  _att_oreg = portOutputRegister(digitalPinToPort(att));
+  _dat_mask = digitalPinToBitMask(dat);
+  _dat_ireg = portInputRegister(digitalPinToPort(dat));
+#endif
+
+  pinMode(clk, OUTPUT); //configure ports
+  pinMode(att, OUTPUT);
+  pinMode(cmd, OUTPUT);
+  pinMode(dat, INPUT);
+
+#if defined(__AVR__)
+  digitalWrite(dat, HIGH); //enable pull-up
+#endif
+
+  CMD_SET(); ;
+  CLK_SET();
+
+  //new error checking. First, read gamepad a few times to see if it's talking
+
+  unsigned long Millis = millis();
+
+  do {
+    read_gamepad();
+  }
+  while (millis() - Millis < 500 && (PS2data[1] != 0x41 && PS2data[1] != 0x73 && PS2data[1] != 0x79));
+
+  NOP;
+  NOP;
+
+  if (PS2data[1] != 0x41 && PS2data[1] != 0x73 && PS2data[1] != 0x79) {
+    Serial.println("Controller mode not matched or no controller found");
+    Serial.print("Expected 0x41, 0x73 or 0x79, but got ");
+    Serial.println(PS2data[1], HEX);
+    return false;
+  }
+  return true; //no error if here
+}
+
+void PS::sendCommandString(byte string[], byte len) {
+  NOP;
+  NOP;
+  ATT_CLR(); // low enable joystick
+  for (int y = 0; y < len; y++)
+    _gamepad_shiftinout(string[y]);
+  ATT_SET(); //high disable joystick
+  delayForMillis(read_delay);                  //wait a few
+  NOP;
+  NOP;
+}
+
+void PS::reconfig_gamepad() {
+  NOP;
+  NOP;
+  sendCommandString(enter_config, sizeof(enter_config));
+  sendCommandString(set_mode, sizeof(set_mode));
+  sendCommandString(exit_config, sizeof(exit_config));
+  NOP;
+  NOP;
+}
+
+unsigned int PS::ButtonDataByte() {
+  return (~buttons);
+}
+
+uint8_t PS::analogRX(void) {
+  if ((PS2data[1] & 0x70) != 0x70)return 127;
+  return PS2data[5];
+}
+uint8_t PS::analogRY(void) {
+  if ((PS2data[1] & 0x70) != 0x70)return 127;
+  return PS2data[6];
+}
+
+uint8_t PS::analogLX(void) {
+  if ((PS2data[1] & 0x70) != 0x70)return 127;
+  return PS2data[7];
+}
+uint8_t PS::analogLY(void) {
+  if ((PS2data[1] & 0x70) != 0x70)return 127;
+  return PS2data[8];
+}
+
+bool PS::pressCross(void) {
+  if ((ButtonDataByte() & PSB_CROSS) > 0 )return true;
+  else return false;
+}
+
+bool PS::pressCircle(void) {
+  if ((ButtonDataByte() & PSB_CIRCLE) > 0 )return true;
+  else return false;
+}
+
+bool PS::pressTriangle(void) {
+  if ((ButtonDataByte() & PSB_TRIANGLE) > 0 )return true;
+  else return false;
+}
+
+bool PS::pressSquare(void) {
+  if ((ButtonDataByte() & PSB_SQUARE) > 0 )return true;
+  else return false;
+}
+
+bool PS::pressR1(void) {
+  if ((ButtonDataByte() & PSB_R1) > 0 )return true;
+  else return false;
+}
+
+bool PS::pressR2(void) {
+  if ((ButtonDataByte() & PSB_R2) > 0 )return true;
+  else return false;
+}
+
+bool PS::pressL1(void) {
+  if ((ButtonDataByte() & PSB_L1) > 0 )return true;
+  else return false;
+}
+
+bool PS::pressL2(void) {
+  if ((ButtonDataByte() & PSB_L2) > 0)return true;
+  else return false;
+}
+
+
+bool PS::pressLeft(void) {
+  if ((ButtonDataByte() & PSB_PAD_LEFT) > 0 )return true;
+  else return false;
+}
+
+bool PS::pressRight(void) {
+  if ((ButtonDataByte() & PSB_PAD_RIGHT) > 0 )return true;
+  else return false;
+}
+
+bool PS::pressUp(void) {
+  if ((ButtonDataByte() & PSB_PAD_UP) > 0 )return true;
+  else return false;
+}
+
+bool PS::pressDown(void) {
+  if ((ButtonDataByte() & PSB_PAD_DOWN) > 0 )return true;
+  else return false;
+}
+
+bool PS::pressStart(void) {
+  if ((ButtonDataByte() & PSB_START) > 0 )return true;
+  else return false;
+}
+
+bool PS::pressSelect(void) {
+  if ((ButtonDataByte() & PSB_START) > 0 )return true;
+  else return false;
+}
---
+++ src/PS.h
@@ -0,0 +1,128 @@
+#ifndef _PS_H_
+#define _PS_H_
+#include "Arduino.h"
+
+#define CTRL_CLK        4
+#define CTRL_BYTE_DELAY 3
+
+//#define PS_COM_DEBUG
+
+#define NOP __asm__ __volatile__ ("nop\n\t")
+
+
+//These are our button constants
+#define PSB_SELECT      0x0001
+#define PSB_L3          0x0002
+#define PSB_R3          0x0004
+#define PSB_START       0x0008
+#define PSB_PAD_UP      0x0010
+#define PSB_PAD_RIGHT   0x0020
+#define PSB_PAD_DOWN    0x0040
+#define PSB_PAD_LEFT    0x0080
+#define PSB_L2          0x0100
+#define PSB_R2          0x0200
+#define PSB_L1          0x0400
+#define PSB_R1          0x0800
+#define PSB_GREEN       0x1000
+#define PSB_RED         0x2000
+#define PSB_BLUE        0x4000
+#define PSB_PINK        0x8000
+#define PSB_TRIANGLE    0x1000
+#define PSB_CIRCLE      0x2000
+#define PSB_CROSS       0x4000
+#define PSB_SQUARE      0x8000
+
+//These are stick values
+#define PSS_RX 5
+#define PSS_RY 6
+#define PSS_LX 7
+#define PSS_LY 8
+
+
+#define SET(x,y) (x|=(1<<y))          //set bit
+#define CLR(x,y) (x&=(~(1<<y)))       //reset bit
+#define CHK(x,y) (x & (1<<y))         //get bit
+#define TOG(x,y) (x^=(1<<y))
+
+class PS
+{
+  public:
+    PS();
+    bool Button(uint16_t);
+    unsigned int ButtonDataByte();
+    boolean  read_gamepad();
+    bool config_gamepad(uint8_t, uint8_t, uint8_t, uint8_t);
+
+
+    uint8_t analogRX(void);
+    uint8_t analogRY(void);
+    uint8_t analogLX(void);
+    uint8_t analogLY(void);
+
+    bool pressCross(void);// Press cross
+    bool pressCircle(void);// Circle press
+    bool pressSquare(void);//  Square press
+    bool pressTriangle(void);//triagle press
+
+    bool pressR1(void);
+    bool pressR2(void);
+    bool pressL1(void);
+    bool pressL2(void);
+
+    bool pressLeft(void);
+    bool pressRight(void);
+    bool pressUp(void);
+    bool pressDown(void);
+
+    bool pressStart(void);
+    bool pressSelect(void);
+
+
+  private:
+
+    void reconfig_gamepad();
+
+    unsigned char PS2data[21];
+    unsigned int buttons;
+
+    inline void CLK_SET(void);
+    inline void CLK_CLR(void);
+    inline void CMD_SET(void);
+    inline void CMD_CLR(void);
+    inline void ATT_SET(void);
+    inline void ATT_CLR(void);
+    inline bool DAT_CHK(void);
+
+    unsigned char _gamepad_shiftinout (char);
+    void sendCommandString(byte*, byte);
+    unsigned int last_buttons;
+
+#ifdef __AVR__
+    uint8_t maskToBitNum(uint8_t);
+    uint8_t _clk_mask;
+    volatile uint8_t *_clk_oreg;
+    uint8_t _cmd_mask;
+    volatile uint8_t *_cmd_oreg;
+    uint8_t _att_mask;
+    volatile uint8_t *_att_oreg;
+    uint8_t _dat_mask;
+    volatile uint8_t *_dat_ireg;
+#else
+
+    uint8_t clk;
+    uint8_t cmd;
+    uint8_t att;
+    uint8_t dat;
+
+#endif
+
+
+
+    unsigned long last_read = 0;
+    byte read_delay;
+
+};
+
+
+#endif
+
